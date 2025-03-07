aa30e19eb16b75ed41fd3eb55a15291384f74a62
PhiLongNLU <21130436@st.hcmuaf.edu.vn>
2025-03-07 10:46:38 +0700

    set up project

--- qa_ui/src/App.tsx
+++ qa_ui/src/App.tsx
@@ -1,9 +1,12 @@
+import Home from "./pages/Home";
 import './App.css'
 
 function App() {
 
     return (
-        <h1 className="text-3xl font-bold">Hello world!</h1>
+        <>
+            <Home/>
+        </>
     )
 }
 
---
+++ qa_ui/src/components/LogIn.tsx
@@ -0,0 +1,9 @@
+const LogIn = () => {
+    return (
+        <>
+            <div>
+                <h4></h4>
+            </div>
+        </>
+    )
+}
