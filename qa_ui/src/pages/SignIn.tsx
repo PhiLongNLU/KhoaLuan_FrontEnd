b8f121686972df4e8b1c132cbaa90b79c66852a7
linhnguyen-dev <linhnguyenchi93147@gmail.com>
2025-03-16 17:34:08 +0700

    done authenticate ui

--- qa_ui/src/pages/SignIn.tsx
+++ qa_ui/src/pages/SignIn.tsx
@@ -6,8 +6,7 @@ import LogIn from "../components/Authenticate/LogIn.tsx";
 const SignIn = () => {
     const [view, setView] = useState<"choose" | "signin" | "signup">("choose");
     return (
-        <div className="flex items-center justify-center w-full h-screen bg-cover bg-center"
-             style={{backgroundImage : "url(../../assets/backgrounds/Sign In.jpg)"}}>
+        <div id={"login-page"} className="flex items-center justify-center w-full h-screen bg-cover bg-center">
             <div className="bg-white p-8 rounded-2xl shadow-lg text-center border-2 border-pink-300">
                 {view === "choose" ? <AuthType onChangeView={setView}/> : null}
                 {view === "signin" ? <LogIn onChangeView={setView}/> : null}
