9d0ad6325bbc9a584825fa0838a83e07f0ada3cb
KhuongVo2105 <khuongvo2105@gmail.com>
2025-08-03 09:06:48 +0700

    feat: :sparkles: create redux api for conversations

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,13 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import { conversationApi } from "@/store/api/conversationApi";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
-  })
-}
+    reducer: {
+      [conversationApi.reducerPath]: conversationApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(conversationApi.middleware),
+  });
+};
 
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>
+export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
