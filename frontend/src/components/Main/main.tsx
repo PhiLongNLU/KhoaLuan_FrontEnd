e86bcf7d45fc9c5ad1a2c65b8ff664f5591b88e0
KhuongVo2105 <khuongvo2105@gmail.com>
2025-08-05 06:06:55 +0700

    chore: update properties 4 components

--- frontend/src/components/Main/main.tsx
+++ frontend/src/components/Main/main.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ConversationList from './Sidebar/ConversationList';
 import ChatWindow from './Chat/ChatWindow';
 import SpinnerOverlay from '../Share/spinnerOverlay';
@@ -7,12 +7,19 @@ import SpinnerOverlay from '../Share/spinnerOverlay';
 const MainPage = () => {
 
   const [isLoading, setIsLoading] = useState(false)
+  const [currentConversation, setCurrentConversation] = useState<string>("")
 
   return (<>
     {isLoading && <SpinnerOverlay />}
     <div className='flex h-screen'>
-      <ConversationList setIsLoading={setIsLoading}/>
-      <ChatWindow />
+      <ConversationList
+        setIsLoading={setIsLoading}
+        setCurrentConversation={setCurrentConversation}
+        currentConversation={currentConversation}
+      />
+      <ChatWindow
+        setIsLoading={setIsLoading}
+        currentConversation={currentConversation} />
     </div>
   </>
   )
